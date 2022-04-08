28f4b639fc72df1a07ebd1e51621844f5df00dd8
JDat99 <jumpingjack1867@gmail.com>
2022-04-08 10:52:52 -0400

    did more sutff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPainter, QBrush, QPen
 from PyQt5 import QtCore
 
 from ping3 import ping
@@ -12,33 +12,52 @@ from ping3 import ping
 app = QApplication(sys.argv) # Initializing GUI
 
 target = '10.2.144.139' # IP address that will be pung
+interval = 500 # Interval that IP will be pung in milliseconds
+width = 220 # Fixed width of the window
+height = 100 # Fixed height of the window
 
 window = QWidget()
 window.setWindowTitle('PiPing')
-window.setGeometry(0, 0, 220, 80)
+window.setFixedSize(width, height)
+
 pingLabel = QLabel("", parent=window)
 hostLabel = QLabel("", parent=window)
+intervalLabel = QLabel("", parent=window)
+colorLabel = QLabel("", parent=window)
 
-pingLabel.setGeometry(10, 5, 220, 35)
-hostLabel.setGeometry(10, 45, 220, 35)
+pingLabel.setGeometry(10, 5, 125, 35)
+hostLabel.setGeometry(10, 45, 220, 20)
+intervalLabel.setGeometry(10, 70, 220, 20)
+colorLabel.setGeometry(165, 5, 50, 50)
 
 hostLabel.setText("target: " + target)
+intervalLabel.setText("interval: " + str(interval) + "ms")
+colorLabel.setStyleSheet("background:gray")
 
 window.show()
 
 def pingUpdate():
-    ms = str(round(ping(target) * 1000))
-    if ms == "None": 
+
+    ms = round(ping(target) * 1000)
+    msStr = str(ms)
+    if msStr == "None": 
         pingLabel.setText("Destination Unreachable")
         print("Destination Unreachable")
         pingLabel.setFont(QFont('Arial', 12))
     else:
-        pingLabel.setText(ms + "ms")
-        print(ms)
+        pingLabel.setText(msStr + "ms")
+        print(msStr)
         pingLabel.setFont(QFont('Arial', 20))
+
+    if ms < 75:
+        colorLabel.setStyleSheet("background:lime")
+    elif ms < 150:
+        colorLabel.setStyleSheet("background:yellow")
+    else:
+        colorLabel.setStyleSheet("background:red")
 
 timer = QtCore.QTimer()
 timer.timeout.connect(pingUpdate)
-timer.start(1000)
+timer.start(interval)
 
 sys.exit(app.exec_())
